f22b817e3afbed4b72f9bcd65175139845a5c8ad
Willian-code <willianrosendodelima@outlook.com>
2022-10-01 16:31:22 -0300

    taking a random word from the giant file

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,13 +1,23 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Hangman {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scanner = new Scanner(new File("/home/user/myprojects/Hangman/world_alpha.txt"));
 
+        List<String> words = new ArrayList<>();
         while (scanner.hasNext()) {
-            System.out.println(scanner.nextLine());
+            words.add(scanner.nextLine());
         }
+
+        Random rand = new Random();
+
+        String word = words.get(rand.nextInt(words.size()));
+
+        System.out.println(word);
     }
 }
