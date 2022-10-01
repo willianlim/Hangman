8fa5aa6779161a4b0ef9629536b6caa119bcd15d
Willian-code <willianrosendodelima@outlook.com>
2022-10-01 16:17:41 -0300

    file that serves as a query for words

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,6 +1,13 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Hangman {
-    public static void main(String[] args) {
-        System.out.println("oi");
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("/home/user/myprojects/Hangman/world_alpha.txt"));
+
+        while (scanner.hasNext()) {
+            System.out.println(scanner.nextLine());
+        }
     }
 }
